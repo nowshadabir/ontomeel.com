d7934497a99da80fad1252e27da4e4744a33abd2
nowshadabir <knabirofficial@gmail.com>
2026-04-02 06:51:38 +0600

    DLT

--- list_cols.php
+++
@@ -1,8 +0,0 @@
-<?php
-include 'includes/db_connect.php';
-$stmt = $pdo->query('DESCRIBE books');
-$rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
-foreach($rows as $row) {
-    echo $row['Field'] . "\n";
-}
-?>
